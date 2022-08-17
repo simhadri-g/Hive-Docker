c87705ffb58813816642f60b447c451443b0cd06
SimhadriGovindappa <simhadri064@gmail.com>
2022-08-17 16:26:33 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -87,6 +87,8 @@ sleep 20
 
 gprn "Start HiveServer2"
 hive/bin/hive --service hiveserver2 --hiveconf hive.server2.thrift.port=10001 --hiveconf hive.execution.engine=mr
+
+#To attach a debugger use --debug as follows. The remote debugger can be attached on port 8000.
 #hive/bin/hive --service hiveserver2 --debug --hiveconf hive.server2.thrift.port=10001 --hiveconf hive.execution.engine=mr
 
 sleep 20000
